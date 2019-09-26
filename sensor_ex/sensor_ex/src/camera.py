78803e88da04a7764b0f208f6b59a65d97c50e93
Kate <49902595+katebrighteyes@users.noreply.github.com>
2019-09-27 08:58:35 +0900

    Update camera.py

--- sensor_ex/sensor_ex/src/camera.py
+++ sensor_ex/sensor_ex/src/camera.py
@@ -7,15 +7,7 @@ from cv_bridge import CvBridge
 
 bridge = CvBridge()
 
-def img_callback(img_data):
-  global bridge
-  global cv_image
-  cv_image = bridge.imgmsg_to_cv2(img_data, "bgr8")
-
-rospy.init_node('Camera_receive_node')
-rospy.Subscriber("/usb_cam/image_raw/", Image, img_callback)
-
-time.sleep(1.5)
+# TODO
 
 while not rospy.is_shutdown():
   cv2.imshow('original', cv_image)
